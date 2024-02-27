4a02406b3e3585d81d5edc93caa3d2726a4b6ca2
JeongSabibi <jsbb528@naver.com>
2024-02-27 23:07:46 +0900

    feat: cardList 컴포넌트 및 pagination 컴포넌트 구현

--- src/components/list/CardList.jsx
+++ src/components/list/CardList.jsx
@@ -1,17 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import CardItem from './CardItem';
+import PagiNation from './PagiNation';
+import { getAllSubject } from 'api';
+import { useSearchParams } from 'react-router-dom';
 
 const Container = styled.section`
-  width: 1200px;
+  max-width: 940px;
   display: grid;
-  grid-template: repeat(4, 1fr) / repeat(2, 1fr);
+  grid-template: repeat(2, 1fr) / repeat(4, 1fr);
   gap: 20px;
+  margin: 0 auto;
 `;
 
 function CardList() {
   const [cards, setCards] = useState(null);
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get('page');
 
-  return <div>CardList</div>;
+  const fetchData = async newPage => {
+    const offset = (newPage - 1) * 8; // 페이지당 8개씩
+    try {
+      const data = await getAllSubject(8, offset, 'time');
+      setCards(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handlePageChange = newPage => {
+    fetchData(newPage);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {cards ? (
+        <>
+          <Container>
+            {cards.results.map(card => (
+              <CardItem key={card.id} {...card} />
+            ))}
+          </Container>
+          <PagiNation
+            totalItems={cards.count} // 데이터의 총 개수
+            itemCountPerPage={8} // 페이지 당 보여줄 데이터 개수
+            pageCount={5} // 보여줄 페이지 개수
+            currentPage={page && parseInt(page) > 0 ? parseInt(page) : 1} // 현재 페이지 3반환
+            onPageChange={handlePageChange} // 페이지 변경 핸들러
+          />
+        </>
+      ) : (
+        <div>질문 대상이 없습니다</div>
+      )}
+    </>
+  );
 }
 
 export default CardList;
