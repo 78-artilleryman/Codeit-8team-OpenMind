561efb4110d5743ae81e68dce261c19e50cdcf3b
Dodam0719 <blueoh719@gmail.com>
2024-02-28 20:36:15 +0900

    design: App.js에서 Title과 Filter를 감싸는 container에 css 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import styled from 'styled-components';
 import Filter from 'components/list/Filter';
 import Title from 'components/list/Title';
 
+const StyledTitleFilterArea = styled.div`
+  width: 21.3125rem;
+  margin: 8.56rem auto 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.75rem;
+`;
+
 function App() {
   return (
-    <div>
+    <StyledTitleFilterArea>
       <Title />
       <Filter />
-    </div>
+    </StyledTitleFilterArea>
   );
 }
 
