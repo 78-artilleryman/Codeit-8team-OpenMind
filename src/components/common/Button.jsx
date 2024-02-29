298a3feabc43c6531dde152a598deace54ebfe96
yohan11 <yipco77@naver.com>
2024-03-01 00:14:32 +0900

    feat: floating 버튼 생성

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -31,6 +31,16 @@ const BasicButton = styled.button`
   }
 `;
 
+const FloatingButton = styled(BasicButton)`
+  position: fixed;
+  box-shadow: 0 4px 4px 0 #00000040;
+  border-radius: 200px;
+
+  bottom: ${({ location }) => location.bottom};
+  top: ${({ location }) => location.top};
+  left: ${({ location }) => location.left};
+  right: ${({ location }) => location.right};
+`;
 const Button = ({
   children,
   className,
@@ -39,11 +49,24 @@ const Button = ({
   bright,
   inactive,
   varient,
+  location = { bottom: '0', top: '0', left: '0', right: '0' },
 }) => {
   return (
     <>
       {varient === 'icon' ? (
         <button onClick={onClick}>{children}</button>
+      ) : varient === 'floating' ? (
+        <FloatingButton
+          onClick={onClick}
+          className={className}
+          width={width}
+          bright={bright}
+          inactive={inactive}
+          disabled={inactive}
+          location={location}
+        >
+          {children}
+        </FloatingButton>
       ) : (
         <BasicButton
           onClick={onClick}
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -3,10 +3,29 @@ import PostBanner from 'components/post/PostBanner';
 import { useParams } from 'react-router-dom';
 import { getSubjectById } from '../api';
 import Share from 'components/post/Share';
+import Button from 'components/common/Button';
+import styled from 'styled-components';
+
+const AddQuestionButton = styled(Button)`
+  @media (max-width: 767px) {
+    width: 123px;
+  }
+`;
 
 const Post = () => {
   const { postId } = useParams();
   const [postData, setPostData] = useState();
+  const [shortButton, setShortButton] = useState(false);
+
+  // 창 크기가 바뀔 때 질문 작성 버튼 문구 변경
+  window.onresize = function () {
+    const screenWidth = window.innerWidth;
+    if (screenWidth <= 767) {
+      setShortButton(true);
+    } else {
+      setShortButton(false);
+    }
+  };
 
   useEffect(() => {
     getSubjectById(postId).then(setPostData);
@@ -21,6 +40,13 @@ const Post = () => {
             userName={postData.name}
           ></PostBanner>
           <Share />
+          <AddQuestionButton
+            varient="floating"
+            width={208}
+            location={{ top: '90%', right: '32px' }}
+          >
+            {shortButton ? '질문작성' : '질문 작성하기'}
+          </AddQuestionButton>
         </>
       )}
     </div>
