0aa79defd10989465add8d4f0495c0be2a39d63b
78-artilleryman <ywy040150@gmail.com>
2024-03-01 19:07:25 +0900

    feat: Loding 컴포넌트 생성 및 적용
    - Card data를 가져오기 전에 띄워줄 Loding 컴포넌트 생성

--- src/components/list/CardList.jsx
+++ src/components/list/CardList.jsx
@@ -5,6 +5,7 @@ import PagiNation from './PagiNation';
 import { getAllSubject } from 'api';
 import { useSearchParams } from 'react-router-dom';
 import useBrowserSize from 'hooks/useBrowserSize';
+import Loding from 'components/common/Loding';
 
 const Container = styled.section`
   max-width: 940px;
@@ -23,9 +24,11 @@ const Container = styled.section`
 
 const CardList = () => {
   const [cards, setCards] = useState(null);
+  const [limit, setLimit] = useState(8);
+  const [isLoading, setIsLoading] = useState(false);
+
   const [searchPage] = useSearchParams();
   const [searchSort] = useSearchParams();
-  const [limit, setLimit] = useState(8);
 
   const page = searchPage.get('page');
   const sort = searchSort.get('sort');
@@ -33,12 +36,15 @@ const CardList = () => {
   const { windowWidth } = useBrowserSize();
 
   const fetchData = async (newPage, sort) => {
+    setIsLoading(true);
     const offset = (newPage - 1) * limit; // 페이지당 limit개씩
     try {
       const data = await getAllSubject(limit, offset, sort);
       setCards(data);
     } catch (error) {
       console.error('Error fetching data:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -64,28 +70,31 @@ const CardList = () => {
     handleMaxCard();
   }, [handleMaxCard]);
 
-  return (
+  if (!cards)
+    return (
+      <>
+        <Loding />
+      </>
+    );
+
+  return isLoading ? (
+    <Loding />
+  ) : (
     <>
-      {cards ? (
-        <>
-          <Container>
-            {cards.results.map(card => (
-              <CardItem key={card.id} {...card} />
-            ))}
-          </Container>
-          <PagiNation
-            totalItems={cards.count} // 데이터의 총 개수
-            itemCountPerPage={limit} // 페이지 당 보여줄 데이터 개수
-            pageCount={5} // 보여줄 페이지 개수
-            currentPage={page && parseInt(page) > 0 ? parseInt(page) : 1} // 현재 페이지 반환
-            onPageChange={handlePageChange} // 페이지 변경 핸들러
-            selectPageNumber={page}
-            sort={sort}
-          />
-        </>
-      ) : (
-        <div>질문 대상이 없습니다</div>
-      )}
+      <Container>
+        {cards.results.map(card => (
+          <CardItem key={card.id} {...card} />
+        ))}
+      </Container>
+      <PagiNation
+        totalItems={cards.count} // 데이터의 총 개수
+        itemCountPerPage={limit} // 페이지 당 보여줄 데이터 개수
+        pageCount={5} // 보여줄 페이지 개수
+        currentPage={page && parseInt(page) > 0 ? parseInt(page) : 1} // 현재 페이지 반환
+        onPageChange={handlePageChange} // 페이지 변경 핸들러
+        selectPageNumber={page}
+        sort={sort}
+      />
     </>
   );
 };
