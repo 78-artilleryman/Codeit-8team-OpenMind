f18b94f7740b02798cec3dcd7482b875f381cfa6
yohan11 <yipco77@naver.com>
2024-03-05 12:33:47 +0900

    design: 버튼 디자인 수정

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,40 +1,36 @@
 import styled from 'styled-components';
 
 const BasicButton = styled.button`
-  width: ${({ width }) => width}px;
-  height: ${({ height }) => (height ? height : 46)}px;
+  width: ${({ $width }) => $width}px;
+  height: ${({ $height }) => ($height ? $height : 46)}px;
   border-radius: 8px;
-  background-color: ${({ bright }) =>
-    bright ? 'var(--brown10)' : 'var(--brown40)'};
+  background-color: ${({ $bright }) =>
+    $bright ? 'var(--brown10)' : 'var(--brown40)'};
   padding: 12px 24px;
 
   font-size: 16px;
   font-weight: 400;
-  color: ${({ bright }) => (bright ? 'var(--brown40)' : 'var(--gray10)')};
+  color: ${({ $bright }) => ($bright ? 'var(--brown40)' : 'var(--gray10)')};
   border: 1px solid var(--brown40);
 
-  opacity: ${({ inactive }) => inactive && 0.5};
+  opacity: ${({ $inactive }) => $inactive && 0.5};
 
   white-space: nowrap;
   display: flex;
   align-items: center;
   justify-content: center;
 
-  cursor: ${({ inactive }) => inactive && 'default'};
+  cursor: ${({ $inactive }) => $inactive && 'default'};
 
-  ${({ inactive }) =>
-    inactive === false &&
-    `
-  &:hover {
+  &:not([disabled]):hover {
     border: 2px solid var(--brown40);
   }
 
-  &:active {
+  &:not([disabled]):active {
     border: 2px solid var(--brown50);
-    background-color: ${({ bright }) =>
-      bright ? 'var(--brown20)' : 'var(--brown50)'};
+    background-color: ${({ $bright }) =>
+      $bright ? 'var(--brown20)' : 'var(--brown50)'};
   }
-  `}
 `;
 
 const FloatingButton = styled(BasicButton)`
@@ -49,7 +45,7 @@ const Button = ({
   width,
   height,
   bright,
-  inactive,
+  inactive = false,
   varient,
 }) => {
   return (
