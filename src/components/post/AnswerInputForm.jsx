dd0ae60940cb09102deba043ccdaa1ee93adee9f
Dodam0719 <blueoh719@gmail.com>
2024-03-08 14:05:12 +0900

    fix: answer에서 답변내용이 실제로 수정되지 않았을 때, '수정 완료'버튼이 비활성화 되도록 수정

--- src/components/post/AnswerInputForm.jsx
+++ src/components/post/AnswerInputForm.jsx
@@ -1,7 +1,7 @@
 // 답변을 생성하고 수정하는 input 컴포넌트
 import styled from 'styled-components';
 import Button from 'components/common/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createAnswer, editAnswer } from 'api';
 
 const Container = styled.div`
@@ -39,12 +39,20 @@ const AnswerInputForm = ({
   placeholder,
   children,
   questionId,
+  originalAnswer = '',
   buttonText,
   type,
   answerId,
   onEditCancel,
 }) => {
-  const [answer, setAnswer] = useState('');
+  // children prop이 전달되면 답변 수정상황으로 판단하여, children을 초기상태로 사용
+  // 그렇지 않다면 답변 생성상황으로 판단하여, 빈 문자열을 초기 상태로 사용
+  const [answer, setAnswer] = useState(children || '');
+
+  // children prop이 변경될 때마다 answer 상태를 업데이트(답변 수정 시 초기 값이 원본 답변으로 설정되도록 함)
+  useEffect(() => {
+    setAnswer(children || '');
+  }, [children]);
 
   const handleChange = e => {
     setAnswer(e.target.value);
@@ -58,16 +66,24 @@ const AnswerInputForm = ({
     editAnswer(answerId, answer).then(() => window.location.reload());
   };
 
+  // 원본 답변과 현재 답변이 동일한지 여부를 체크
+  const isAnswerUnchanged = originalAnswer.trim() === answer.trim();
+
   return (
     <Container>
-      <StyledTextArea placeholder={placeholder} onChange={handleChange}>
+      <StyledTextArea
+        placeholder={placeholder}
+        value={answer}
+        onChange={handleChange}
+      >
         {children}
       </StyledTextArea>
       {/*답변을 생성하는 input인 경우 '답변 완료' 버튼 하나, 답변을 수정하는 input인 경우 '수정완료' '수정취소' 두개의 버튼이 나타납니다.*/}
       {/*답변을 생성하는 input인 경우 handleCreateAnswer, 답변을 수정하는 input인 경우 handleEditAnswer 함수를 실행합니다.*/}
       <ButtonContainer>
         <Button
-          inactive={answer.trim() === ''}
+          // 답변이 비어있거나 변경되지 않았을 때 '수정 완료'버튼 비활성화
+          inactive={answer.trim() === '' || isAnswerUnchanged}
           onClick={
             type === 'create answer' ? handleCreateAnswer : handleEditAnswer
           }
