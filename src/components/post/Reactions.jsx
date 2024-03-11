ae39de563e0f96a26cbc963eb49c00620827b6f5
yohan11 <yipco77@naver.com>
2024-03-11 22:10:53 +0900

    fix: 좋아요/싫어요 새로고침 시 이전 기록 남아있도록 수정

--- src/components/post/Reactions.jsx
+++ src/components/post/Reactions.jsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from 'components/common/Button';
 import * as Icons from 'components/common/Icons';
-import { postQuestionsReaction } from '../../api';
+import { getQuestionsById, postQuestionsReaction } from '../../api';
+import {
+  setDislikeLocalStorage,
+  setLikeLocalStorage,
+} from 'utils/useLocalStorage';
 
 const ButtonsContainer = styled.div`
   display: flex;
@@ -15,33 +19,69 @@ const LikeButton = styled(Button)`
   display: flex;
   gap: 6px;
   align-items: center;
-  color: ${({ $clicked }) => ($clicked ? 'var(--blue)' : 'var(--gray40ToGray20)')};
+  color: ${({ $clicked }) =>
+    $clicked ? 'var(--blue)' : 'var(--gray40ToGray20)'};
 `;
 
-const Reactions = ({ qnaData }) => {
+const Reactions = ({ qnaData, setPostData, postId }) => {
   const [likeClicked, setLikeClicked] = useState(false);
   const [dislikeClicked, setDislikeClicked] = useState(false);
 
   const handleLike = () => {
     if (!likeClicked) {
-      postQuestionsReaction(qnaData.id, 'like');
-      setLikeClicked(true);
+      postQuestionsReaction(qnaData.id, 'like')
+        .then(() => getQuestionsById(postId))
+        .then(res => {
+          const { results } = res;
+          setPostData(() => results);
+          setLikeLocalStorage(qnaData.id);
+          setLikeClicked(true);
+        })
+        .catch(error => {
+          // 오류 처리
+          console.error('답변을 수정하는데 문제가 생겼습니다', error);
+        });
     }
   };
 
   const handleDislike = () => {
     if (!dislikeClicked) {
-      postQuestionsReaction(qnaData.id, 'dislike');
-      setDislikeClicked(true);
+      postQuestionsReaction(qnaData.id, 'dislike')
+        .then(() => getQuestionsById(postId))
+        .then(res => {
+          const { results } = res;
+          setPostData(() => results);
+          setDislikeLocalStorage(qnaData.id);
+          setDislikeClicked(true);
+        })
+        .catch(error => {
+          // 오류 처리
+          console.error('답변을 수정하는데 문제가 생겼습니다', error);
+        });
     }
   };
+
+  useEffect(() => {
+    if (
+      localStorage.getItem('like') &&
+      localStorage.getItem('like').includes(qnaData.id)
+    ) {
+      setLikeClicked(true);
+    }
+    if (
+      localStorage.getItem('dislike') &&
+      localStorage.getItem('dislike').includes(qnaData.id)
+    ) {
+      setDislikeClicked(true);
+    }
+  }, []);
 
   if (!qnaData) return <></>;
   return (
     <ButtonsContainer>
       <LikeButton varient="icon" onClick={handleLike} $clicked={likeClicked}>
         <Icons.ThumbsUp $clicked={likeClicked} />
-        좋아요 {likeClicked && qnaData.like + 1}
+        좋아요 {likeClicked && qnaData.like}
       </LikeButton>
       <LikeButton
         varient="icon"
@@ -49,7 +89,7 @@ const Reactions = ({ qnaData }) => {
         $clicked={dislikeClicked}
       >
         <Icons.ThumbsDown $clicked={dislikeClicked} />
-        싫어요 {dislikeClicked && qnaData.dislike + 1}
+        싫어요 {dislikeClicked && qnaData.dislike}
       </LikeButton>
     </ButtonsContainer>
   );
